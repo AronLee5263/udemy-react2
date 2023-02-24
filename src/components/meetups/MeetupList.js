9730dbeab9509edc547e190df7e7690872604ef0
aron 데스크탑 <aronlee5263@gmail.com>
2023-02-24 17:34:00 +0900

    주석 삭제

--- src/components/meetups/MeetupList.js
+++ src/components/meetups/MeetupList.js
@@ -19,26 +19,3 @@ function MeetupList(props) {
 }
 
 export default MeetupList;
-
-/**
- * 위 코드는 MeetupList 컴포넌트입니다. 이 컴포넌트는 Meetup 데이터를 받아와서, 
- * Meetup 목록을 렌더링하는 역할을 합니다.
-
-MeetupList 컴포넌트는 props를 매개변수로 받아, props.meetups 배열에 있는 Meetup 데이터를 사용합니다.
- props.meetups는 AllMeetupsPage 컴포넌트에서 전달한 DUMMY_DATA 배열입니다.
-
-MeetupList 컴포넌트는 MeetupItem 컴포넌트를 렌더링합니다. 
-MeetupItem 컴포넌트는 하나의 Meetup을 나타내는 컴포넌트이며, 
-MeetupList 컴포넌트에서 Meetup 데이터를 전달받아 사용합니다. 
-
-MeetupList 컴포넌트에서는 props.meetups 배열을 map 함수를 사용하여 
-MeetupItem 컴포넌트의 여러 인스턴스를 생성합니다.
-
-매핑된 MeetupItem 컴포넌트의 props는 meetup 객체의 속성들로 설정됩니다.
- meetup 객체는 props.meetups 배열의 요소로부터 비구조화할당을 사용하여 추출됩니다. 
- 이렇게 MeetupItem 컴포넌트에 전달된 props는, 각각의 Meetup을 나타내는 데 사용됩니다.
-
-마지막으로, MeetupList 컴포넌트는 <ul> 태그로 Meetup 목록을 렌더링합니다. 
-MeetupItem 컴포넌트에서 전달받은 props를 사용하여, 하나의 Meetup을 나타내는 <li> 태그를 렌더링합니다.
- 이렇게 MeetupList 컴포넌트는 Meetup 목록을 렌더링하는 역할을 수행하게 됩니다.
- */
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -33,17 +33,4 @@ function AllMeetupsPage() {
 export default AllMeetupsPage;
 // test
 
-/*
 
-DUMMY_DATA 상수는 Meetup 데이터를 담고 있는 배열입니다. 
-이 배열은 MeetupList 컴포넌트에 meetups라는 이름의 props로 전달됩니다.
-
-MeetupList 컴포넌트는 meetups라는 props를 받아, Meetup 데이터를 사용하여 목록을 렌더링합니다.
- MeetupList 컴포넌트가 어떻게 구현되었는지는 코드에서는 알 수 없습니다.
-
-AllMeetupsPage 컴포넌트에서는 h1 태그와 MeetupList 컴포넌트를 렌더링하고 있습니다. 
-MeetupList 컴포넌트에 meetups={DUMMY_DATA}와 같이 props를 전달하면, 
-MeetupList 컴포넌트에서는 전달된 DUMMY_DATA를 사용하여 목록을 렌더링합니다.
-
-따라서, 위 코드는 Meetup 데이터를 담고 있는 DUMMY_DATA 배열을 MeetupList 컴포넌트의 meetups props로 전달하여, 
-Meetup 목록을 렌더링하는 예시입니다.*/
